18ba3f929148057e446828154c7f5d0426e02dbc
Noah Malchy <noahmalchy@gmail.com>
2020-10-11 14:53:40 -0700

    Can now select a genre, and list movies from selected genre

--- src/components/MovieForm.tsx
+++ src/components/MovieForm.tsx
@@ -3,18 +3,128 @@ import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
-import GenreSelect from './GenreSelect';
+import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Typography from '@material-ui/core/Typography';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import axios from 'axios';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+    title: {
+      margin: theme.spacing(4, 0, 2),
+    },
+  }),
+);
 
 const MovieForm = () => {
+
+  const classes = useStyles();
+  const [genreChoice, setGenreChoice] = useState(0);
+  const [genreList, setGenreList] = useState([]);
+  const [movieApiUrl, setMovieApiUrl] = useState('');
+  const [movieList, setMovieList] = useState([]);
+
+
+  const getGenres = async () => {
+    await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US`)
+          .then(response => setGenreList(response.data.genres))
+          .catch((error) => console.log('getGenres HTTP GET Request Error response: ', error))
+  }
+
+  const handleGenreChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setGenreChoice(event.target.value as number);
+  };
+
+  const handleGetMoviesClick = async () => {
+    await axios.get(movieApiUrl)
+          .then(response => {
+            console.log(response.data.results)
+            setMovieList(response.data.results)
+          })
+          .catch((error) => console.log('handleGetMoviesClick HTTP GET Request Error response: ', error))
+  };
+
+  useEffect((): void => {
+    getGenres()
+  }, [])
+
+  useEffect((): void => {
+    setMovieApiUrl(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genreChoice}`);
+    console.log('New Movie url: ', movieApiUrl);
+  }, [genreChoice, movieApiUrl])
+
   return (
     <>
     <h1>Movie Suggester</h1>
     <Paper elevation={3}>
       <Box p={10}>
         <Card>
-          <GenreSelect></GenreSelect>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+            <FormControl variant="outlined" className={classes.formControl}>
+              <InputLabel htmlFor="genre-select-label">Genres</InputLabel>
+              <Select
+                labelId="genre-select-label"
+                id="genre-select"
+                value={genreChoice}
+                onChange={handleGenreChange}
+                label="Genres"
+              >
+                {(genreList || []).map((genre: {name: string, id: number}) => {
+                  return <MenuItem key={genre.id} value={genre.id}>{genre.name}</MenuItem>
+                })}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Button onClick={handleGetMoviesClick}>Find me a movie!</Button>
+          { movieList.length > 0 &&
+            <Grid 
+              container 
+              spacing={2}
+              justify="center"
+              alignItems="center"
+            >
+              <Grid item xs={12} md={6}>
+                <Typography variant="h6" className={classes.title}>
+                  Movies
+                </Typography>
+                <List>
+                  {movieList.map((movie) =>
+                    <ListItem>
+                      <ListItemAvatar>
+                        <Avatar>
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary={movie['title']}
+                        secondary={movie['overview']}
+                      />
+                    </ListItem>,
+                  )}
+                </List>
+              </Grid>
+            </Grid>
+          }
         </Card>
       </Box>
     </Paper>
--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 import App from './App';
-import configureStore from './store/store';
 
-const store = configureStore();
 
 ReactDOM.render(
   <>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-        <Router>
-          <App />
-        </Router>
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
   </>,
   document.getElementById('root')
 );
